e35ca5e8a62218dfeb0aa077f5c587337bce5ba6
jacqueline <D00233669@student.dkit.ie>
2021-03-19 14:05:29 +0000

    added contact-form

--- includes/footer.php
+++ includes/footer.php
@@ -2,6 +2,14 @@
     <p>&copy; <?php echo date("Y"); ?> WackyBone Pet Supplies<img src=".\images\paw.png" alt="paw logo" width="20" height="20"></p>
 </footer>
 </div><!-- close div container -->
+
+<script language="JavaScript">
+		var frmvalidator = new Validator("contactform");
+		frmvalidator.addValidation("name", "req", "Please provide your name");
+		frmvalidator.addValidation("email", "req", "Please provide your email");
+		frmvalidator.addValidation("email", "email", "Please enter a valid email address");
+	</script>
+
 </body>
 
 <script src="ca_validation.js"></script>
